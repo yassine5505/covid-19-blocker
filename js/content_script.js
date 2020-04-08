9453661f0ba328f0b8ed20527e8fded1d3eecf96
Mohammed Yassine Chraibi <med.yassine@beebay.co>
2020-04-08 10:54:43 +0100

    Selecting feed divs with regex and looping through their children

--- js/content_script.js
+++ js/content_script.js
@@ -1,5 +1,20 @@
-$(function() {
-    console.log('Hello World');
-    selectedElement = $("body").contents(); 
-    $("h1").text("hello world");
+$(document).ready(function() {
+    // Get all post divs
+    var articleDivs = $("div[data-pagelet*='Feed']");
+    var articles = Object.values(articleDivs);
+    articles.forEach(article => {
+        console.log(article);
+        if(article.has("p") || article.has("div") || article.has("span")) {
+            var articleChildren = article.children();
+            articleChildren.forEach(child => {
+                // Check if text
+                if(child.is("p") || child.is("span") || child.is("div")) {
+                    if(child.innerText.toLowerCase().includes("covid")) {
+                        console.log("removing article: " + article);
+                        article.remove();
+                    }
+                }
+            })
+        }
+    });
 });
